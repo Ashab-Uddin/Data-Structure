6dbedfd3ec38541f96c87121c9dcf6574f901ea4
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-27 13:50:36 +0600

    Move all file in Singly linked list folder

---
+++ linkedList/SinglyLinkedList/mainImple.c
@@ -0,0 +1,199 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the node structure
+struct node {
+    int data;
+    struct node *next;
+};
+
+// Global head pointer
+struct node *head = NULL;
+
+// Function to create and add a new node to the linked list
+void createNode() {
+    struct node *newnode, *temp;
+    newnode = (struct node*)malloc(sizeof(struct node));
+    
+
+    printf("Enter the data: ");
+    scanf("%d", &newnode->data);
+    newnode->next = NULL;
+
+    if (head == NULL) {
+        head =  newnode; 
+    } else {
+        temp = head;
+        while (temp->next != NULL) {  
+            temp = temp->next;
+        }
+        temp->next = newnode; 
+    }
+}
+void insertFirst(){
+    struct node *newnode;
+    newnode = (struct node*)malloc(sizeof(struct node));
+    printf("Enter data you want to insert: ");
+    scanf("%d",&newnode->data);
+    newnode->next = head;
+    head = newnode;
+}
+void insertEnd(){
+    struct node *newnode,*temp;
+    newnode = (struct node*)malloc(sizeof(struct node));
+    printf("Enter data you want to insert: ");
+    scanf("%d",&newnode->data);
+    newnode->next = NULL;
+    temp = head;
+
+    while(temp->next != 0){
+        temp = temp->next;
+    }
+    temp->next = newnode;
+
+}
+void insertSpecificPosition(){
+    int pos, i = 1;
+    struct node *newnode, *temp;
+
+    newnode = (struct node*)malloc(sizeof(struct node));
+
+    printf("Enter the position to insert the new node: ");
+    scanf("%d", &pos);
+    printf("Enter data you want to insert at position %d: ", pos);
+    scanf("%d", &newnode->data);
+
+    if (pos == 1) {
+        newnode->next = head;
+        head = newnode;
+        return;
+    }
+
+    temp = head;
+    while (i < pos - 1 && temp != NULL) {
+        temp = temp->next;
+        i++;
+    }
+
+    if (temp == NULL) {
+        printf("Position out of range.\n");
+        free(newnode);
+    } else {
+        newnode->next = temp->next;
+        temp->next = newnode;
+    }
+}
+
+void DeleteFromBeg(){
+    struct node *temp;
+    if(head == NULL){
+        printf("The list is empty.\n");
+    }
+    else{
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+void DeleteEnd(){
+    struct node *temp,*prevnode;
+    temp = head;
+    while(temp->next != 0){
+        prevnode = temp;
+        temp = temp->next;
+    }
+    if(temp == head){
+        head = NULL;
+    }
+    else{
+        prevnode->next = NULL;
+    }
+    free(temp);
+}
+void DeleteAny(){
+    struct node *temp,*nextnode;
+    int pos,i=1;
+    printf("Enter the position to Delete the node: ");
+    scanf("%d", &pos);
+    temp = head;
+
+    while(i<pos-1){
+        temp = temp->next;
+        i++;
+    }
+    nextnode = temp->next;
+    temp->next = nextnode->next;
+    free(nextnode);
+}
+
+// Function to display the linked list
+void displayList() {
+    struct node *temp = head;
+    if (temp == NULL) {
+        printf("The list is empty.\n");
+        return;
+    }
+    
+    printf("Linked list: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int choice;
+
+    do {
+        printf("\nMenu:\n");
+        printf("1. Create new node\n");
+        printf("2. Insert at the beginning\n");
+        printf("3. Insert at the End\n");
+        printf("4. Insert at a specific position\n");
+        printf("5. Delete from beginning\n");
+        printf("6. Delete from End\n");
+        printf("7. Delete at a specific position\n");
+        printf("8. Display List\n");
+        printf("0. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice); 
+
+        switch (choice) {
+            case 1:
+                createNode();
+                break;
+            case 2:
+                insertFirst();
+                break;
+            case 3:
+                insertEnd();
+                break;
+            case 4:
+                insertSpecificPosition();
+                break;
+            case 5:
+                DeleteFromBeg();
+                break;
+            case 6:
+                DeleteEnd();
+                break;
+            case 7:
+                DeleteAny();
+                break;
+            case 8:
+                displayList();
+                break;
+            case 0:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    } while (choice != 0); 
+
+
+    displayList();  
+    
+    return 0;
+}
