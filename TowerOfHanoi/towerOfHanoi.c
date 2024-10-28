af1cf6873d8e7661cd9d70e8f5e823fd0a025a48
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-29 02:58:08 +0600

    wap for tower of hanoi

--- TowerOfHanoi/towerOfHanoi.c
+++ TowerOfHanoi/towerOfHanoi.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-void towerOfHanoi(int n, char from, char to, char aux) {
-    if (n == 0) return;
-    towerOfHanoi(n - 1, from, aux, to);
-    printf("Move disk %d from %c to %c\n", n, from, to);
-    towerOfHanoi(n - 1, aux, to, from);
+void towerOfHanoi(int N, char BEG, char AUX, char END) {
+    if (N == 0) return;
+    towerOfHanoi(N - 1, BEG, END, AUX);
+    printf("Move disk %d from %c to %c\n", N, BEG, END);  // Corrected here
+    towerOfHanoi(N - 1, AUX, BEG, END);
 }
 
 int main() {
-    int n;
+    int N;
     printf("Enter the number of disks: ");
-    scanf("%d", &n);
-    printf("Tower of Hanoi solution for %d disks:\n", n);
-    towerOfHanoi(n, 'A', 'C', 'B');
+    scanf("%d", &N);
+    printf("Tower of Hanoi solution for %d disks:\n", N);
+    towerOfHanoi(N, 'A', 'B', 'C');  // Correct order for BEG, AUX, END
     return 0;
 }
