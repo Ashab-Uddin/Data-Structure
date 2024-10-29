ee015fa183d6d18865c46be1721b7075b3a1f0a7
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-29 09:30:05 +0600

    reducd comment

--- TowerOfHanoi/towerOfHanoi.c
+++ TowerOfHanoi/towerOfHanoi.c
@@ -3,7 +3,7 @@
 void towerOfHanoi(int N, char BEG, char AUX, char END) {
     if (N == 0) return;
     towerOfHanoi(N - 1, BEG, END, AUX);
-    printf("Move disk %d from %c to %c\n", N, BEG, END);  // Corrected here
+    printf("Move disk %d from %c to %c\n", N, BEG, END);  
     towerOfHanoi(N - 1, AUX, BEG, END);
 }
 
@@ -12,6 +12,6 @@ int main() {
     printf("Enter the number of disks: ");
     scanf("%d", &N);
     printf("Tower of Hanoi solution for %d disks:\n", N);
-    towerOfHanoi(N, 'A', 'B', 'C');  // Correct order for BEG, AUX, END
+    towerOfHanoi(N, 'A', 'B', 'C'); 
     return 0;
 }
