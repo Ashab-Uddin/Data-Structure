5a81f349e7f38f5809609278bef6eca061299d59
Ashab Uddin <ashabuddin47@gmail.com>
2024-12-18 12:18:20 +0600

    Graph

---
+++ Graph/tempCodeRunnerFile.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#define Nov 5
+void AddEdge(int Graph[][NoV], int i,int j){
+    Graph[i][j] = 1;
+    Graph[j][i] = 1;
+}
+void Display(int Graph[][NoV]){
+    for(int i = 0;i<NoV;i++){
+        for (int j = 0;j<NoV; j++){
+            printf("%d "Graph[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int Graph[NoV][NoV] = {0};
+    AddEdge (Graph, 0, 1);
+    AddEdge (Graph, 0, 2);
+    AddEdge (Graph, 1, 2);
+    AddEdge (Graph, 1, 3);
+    AddEdge (Graph, 1, 4);
+    AddEdge (Graph, 2, 4);
+    AddEdge (Graph, 3, 4);
+    Display(Graph);
+
+
+}
